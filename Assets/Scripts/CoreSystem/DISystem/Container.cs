b88ab2dcbfb3be9a6a890f50c896593dcac2a305
Moosan <washer2016@outlook.jp>
2020-02-21 03:23:45 +0900

    DIContainer

---
+++ Assets/Scripts/CoreSystem/DISystem/Container.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace CoreSystem.DISystem
+{
+    public class Container
+    {
+        private Dictionary<Type, BindInfo> BindInfos { get; } = new Dictionary<Type, BindInfo>();
+        public void Bind<T>(BindInfo<T>.CreateDelegate createDelegate) where T : class
+        {
+            var key = typeof(T);
+            if (BindInfos.ContainsKey(key))
+            {
+                Debug.Log(key + " is already binded.");
+                // TODO bind type
+                return;
+            }
+            BindInfos.Add(typeof(T),new BindInfo<T>(createDelegate));
+        }
+
+        public T Get<T>() where T : class
+        {
+            var key = typeof(T);
+            if (!BindInfos.ContainsKey(key))
+            {
+                Debug.Log(key + " is not binded.");
+                return null;
+            }
+            return ((BindInfo<T>)BindInfos[typeof(T)]).Get(this);
+        }
+
+        public abstract class BindInfo
+        {
+
+        }
+        public class BindInfo<T> : BindInfo where T : class
+        {
+            public delegate T CreateDelegate(Container container);
+            private CreateDelegate Create { get; }
+            private T BindObj { get; set; }
+            private bool Created = false;
+            public BindInfo(CreateDelegate createDelegate)
+            {
+                Create = createDelegate;
+            }
+            public T Get(Container container)
+            {
+                if (!Created)
+                {
+                    BindObj = Create(container);
+                    Created = true;
+                }
+                return BindObj;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CoreSystem/DISystem/Sample.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+namespace CoreSystem.DISystem.Sample
+{
+    public class Sample : MonoBehaviour
+    {
+        private void Awake()
+        {
+            var container = new Container();
+            container.Bind(c => new TestLog("test"));
+            container.Bind(c => new TestA(c.Get<TestLog>()));
+            container.Bind(c => new TestB(c.Get<TestA>(), c.Get<TestLog>()));
+
+            var testB = container.Get<TestB>();
+        }
+    }
+
+    public class TestLog
+    {
+        public string Log { get; }
+        public TestLog(string log)
+        {
+            Log = log;
+        }
+    }
+
+    public class TestA
+    {
+        public TestA(TestLog testLog)
+        {
+
+        }
+    }
+
+    public class TestB
+    {
+        public TestB(TestA testA,TestLog testLog)
+        {
+
+        }
+    }
+}
